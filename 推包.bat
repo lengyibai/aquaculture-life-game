e94d971b29a42fd10ed4b088fcf2f1fcf0f52156
lengyibai <1329670984@qq.com>
2025-12-06 15:34:08 +0800

    .

---
+++ 推包.bat
@@ -0,0 +1,32 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+cd /d "E:\公司\hotupdatatest"
+
+echo 执行 git add . ...
+git add .
+
+echo.
+
+echo 执行 git commit
+git commit -m "."
+
+echo.
+
+echo 执行 git pull ...
+git pull
+
+echo.
+
+echo 请按任意键继续执行 git push ...
+pause
+
+echo 执行 git push ...
+git push
+
+echo.
+
+echo 完成！
+
+pause
